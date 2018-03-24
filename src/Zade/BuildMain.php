61e75a3de858d1edeade0864d78323458665fab7
Zadezter <33807566+Zadezter@users.noreply.github.com>
2018-03-24 13:19:46 +0000

    Fixed.

--- src/Zade/BuildMain.php
+++ src/Zade/BuildMain.php
@@ -359,7 +359,7 @@ class BuildMain extends PluginBase implements Listener {
 			case "buildcreate":
                             if($player->isOp())
                             {
-                                if(!empty($args[0])
+                                if(!empty($args[0]))
 				{
                                     if(file_exists($this->getServer()->getDataPath() . "/worlds/" . $args[0]))
                                     {
